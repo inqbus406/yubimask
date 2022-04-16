7e96e3c2d334ebe5a5af137bd4f5cf5695e356c1
Jess Olmstead <jessolm@cs.washington.edu>
2022-04-16 12:42:46 -0700

    Now able to give a challenge and get a response from a yubikey!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,37 @@
+use std::ops::Deref;
+use yubico_manager::Yubico;
+use yubico_manager::config::{Config, Mode, Slot};
+
 mod ethereum;
 
 fn main() {
     println!("Welcome to YubiMask!");
+    get_yk_response();
+}
+
+fn get_yk_response() {
+    let mut yubi = Yubico::new();
+    if let Ok(device) = yubi.find_yubikey() {
+        println!("Vendor ID: {:?}", device.vendor_id);
+        println!("Product ID: {:?}", device.product_id);
+
+        let config = Config::default()
+            .set_vendor_id(device.vendor_id)
+            .set_product_id(device.product_id)
+            .set_variable_size(true)
+            .set_mode(Mode::Sha1)
+            .set_slot(Slot::Slot2);
+
+        let challenge = String::from("mychallenge");
+        let hmac_result = yubi.challenge_response_hmac(challenge.as_bytes(), config)
+            .unwrap();
+
+        let hexval = hmac_result.deref();
+        println!("{:?}", hmac_result);
+        let hexstring = hex::encode(hexval);
+        println!("Response: {}", hexstring);
+
+    } else {
+        println!("Yubikey not found.");
+    }
 }
