a988d77f68e752d5df93bbeb0609a5f5a72286ed
Jess Olmstead <jessolm@cs.washington.edu>
2022-04-15 17:29:44 -0700

    Basic setup

---
+++ src/ethereum.rs
@@ -0,0 +1,7 @@
+// A wallet module specifically for the Ethereum network.
+// We might want to make a "wallets" module later to include all
+// networks if we add more.
+
+pub fn gen_keypair() {
+    todo!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod ethereum;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to YubiMask!");
 }
