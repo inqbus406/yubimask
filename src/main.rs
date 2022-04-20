fb4fab5afff0e20eb600d9b998aadace48d4cabb
jolmstead <jess.olmstead@nelsonnwalaska.com>
2022-04-19 17:37:49 -0700

    YubiMask sent its first transaction!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-extern crate core;
+//extern crate core; // What tf is this and where did it come from? Seems to work without it??
 
 use std::env;
 use std::io::Write;
+use std::str::FromStr;
 use anyhow::Result;
 use clap::Parser;
+use secp256k1::{key, SecretKey};
+
+// For ethereum
+use web3::types::Address;
 
 mod wallet;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Welcome to YubiMask!");
     let args = Args::parse();
 
@@ -50,7 +56,28 @@ fn main() -> Result<()> {
             Err(e) => panic!("File not found! {}", e)
         };
         println!("Read from file: {:?}", &wallet);
-        println!("Decrypted secret key: {:?}", &mut wallet.get_sec_key(args.debug).unwrap())
+        let sec_key = wallet.get_sec_key(args.debug)?;
+        println!("Decrypted secret key: {:?}", sec_key);
+
+        // Connect to the network
+        let conn = wallet::ethereum::connect().await?;
+
+        // Print out the current block number
+        let block = conn.eth().block_number().await?;
+        println!("block number: {}", &block);
+
+        // Print out the wallet balance
+        let balance = wallet::ethereum::get_balance_eth(&wallet, &conn).await?;
+        println!("Wallet balance: {} ETH", &balance);
+
+        if args.send {
+            let to_addr = Address::from_str("0x5841eb5ccb285C262AD4d9A4144f63B5358DB54e")?;
+            let amount = 0.001;
+            let txn = wallet::ethereum::create_txn(to_addr, amount);
+
+            let txn_hash = wallet::ethereum::sign_and_send(&conn, txn, &sec_key).await?;
+            println!("Transaction hash: {:?}", txn_hash);
+        }
     }
     //wallet::Wallet::encrypt_decrypt("test_message_string");
 
@@ -72,6 +99,10 @@ struct Args {
     /// Start GUI
     #[clap(short, long)]
     vis: bool,
+
+    /// Send (default 0.001 ETH)
+    #[clap(short, long)]
+    send: bool,
 
     /// Debug mode (no yubikey)
     #[clap(short, long)]
--- src/wallet/ethereum.rs
+++ src/wallet/ethereum.rs
@@ -3,12 +3,23 @@
 // networks if we add more.
 // A substantial portion of this code is taken from https://tms-dev-blog.com/build-a-crypto-wallet-using-rust/
 
+use std::str::FromStr;
 use secp256k1::{PublicKey, SecretKey};
 use secp256k1::rand::{rngs, SeedableRng};
-use web3::types::Address;
 use tiny_keccak::keccak256;
 use rand::RngCore;
 use rand::rngs::{OsRng, EntropyRng}; // May want to use EntropyRing as a fallback
+use web3::{transports, Web3};
+use web3::transports::WebSocket;
+use web3::types::{Address, H256, TransactionParameters, U256};
+use anyhow::Result;
+use crate::wallet::Wallet;
+
+// For connecting to the ETH network
+const ETH_MAINNET_ENDPOINT: &str = "wss://mainnet.infura.io/ws/v3/465e5058a793440bb743994f856841af";
+const ETH_RINKEBY_ENDPOINT: &str = "wss://rinkeby.infura.io/ws/v3/465e5058a793440bb743994f856841af";
+const INFURA_PROJECT_ID: &str = "465e5058a793440bb743994f856841af";
+const INFURA_PROJECT_SECRET: &str = "adfcf1aac28349c4a67cd80b04287e91"; // Probably shouldn't have this in plaintext...
 
 pub fn gen_keypair() -> (SecretKey, PublicKey) {
     let secp = secp256k1::Secp256k1::new();
@@ -38,4 +49,45 @@ pub fn address_from_pubkey(pub_key: &PublicKey) -> Address {
     let hash = keccak256(&pub_key[1..]);
 
     Address::from_slice(&hash[12..])
+}
+
+pub async fn connect() -> Result<Web3<WebSocket>> {
+    let url = ETH_RINKEBY_ENDPOINT;
+    let conn = transports::WebSocket::new(url).await?;
+    Ok(Web3::new(conn))
+}
+
+// Returns the balance in wei
+pub async fn get_balance(wallet: &Wallet, conn: &Web3<WebSocket>) -> Result<U256> {
+    let addr = Address::from_str(&wallet.addr)?;
+    Ok(conn.eth().balance(addr, None).await?)
+}
+
+pub async fn get_balance_eth(wallet: &Wallet, conn: &Web3<WebSocket>) -> Result<f64> {
+    Ok(wei_to_eth(get_balance(wallet, conn).await?))
+}
+
+pub fn create_txn(addr: Address, eth: f64) -> TransactionParameters {
+    TransactionParameters {
+        to: Some(addr),
+        value: eth_to_wei(eth),
+        ..Default::default()
+    }
+}
+
+pub async fn sign_and_send(conn: &Web3<WebSocket>, txn: TransactionParameters, sec_key: &SecretKey) -> Result<H256> {
+    let txn = conn.accounts().sign_transaction(txn, sec_key).await?;
+    let result = conn.eth().send_raw_transaction(txn.raw_transaction).await?;
+    Ok(result)
+}
+
+pub fn wei_to_eth(wei: U256) -> f64 {
+    // We're losing some precision here
+    let wei = wei.as_u128() as f64;
+    wei / 1_000_000_000_000_000_000.0
+}
+
+pub fn eth_to_wei(eth: f64) -> U256 {
+    let wei = eth * 1_000_000_000_000_000_000.0;
+    U256::from(wei as u128)
 }
